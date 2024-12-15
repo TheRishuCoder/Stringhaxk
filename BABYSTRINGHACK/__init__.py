b6d580c6b39ed424bd10b43db4edca83973a9520
Rishabh <152057079+Rishabhopi@users.noreply.github.com>
2024-12-16 04:32:29 +0530

    Update __init__.py

--- BABYSTRINGHACK/__init__.py
+++ BABYSTRINGHACK/__init__.py
@@ -17,7 +17,7 @@ START_PIC = Config.START_PIC
 
 
 if not START_PIC:
-    START_PIC = "https://telegra.ph/file/82ebb45ae27cdb244ebd0.jpg"
+    START_PIC = "https://envs.sh/kU9.jpg"
 
 #rich
 LOG = Console()
@@ -39,7 +39,7 @@ async def BABYSTRINGHACK():
     header = Table(show_header=True, header_style="bold green")
     header.add_column(LOG_TEXT)
     LOG.print(header)
-    LOG.print(f"[bold cyan]𝐌𝐑.𝐔𝐓𝐓𝐀𝐌")
+    LOG.print(f"[bold cyan]⛩️ 𝐌𝐑 ⛩️")
     LOG.print("[bold yellow]𝐘𝐨𝐮𝐫 𝐁𝐨𝐭 𝐒𝐭𝐚𝐫𝐭.............")
     await app.start()    
     
