46584fa51901ceb9cb918077c6709914ed203a36
Rishabh <152057079+Rishabhopi@users.noreply.github.com>
2024-12-16 04:36:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
-# TEAM BABY ALL COPYRIGHT ©️
+# TEAM RISHU ALL COPYRIGHT ©️
 from pyrogram import filters
 import os
 
 class Config:
     API_ID = "20948838"
     API_HASH = "0d81af6bc752fd069824375a9d668839"
-    #TOKEN = "6521122303:AAGCO3XMjcA0SN5NAi1M0NpmbmMxEtwwYbg"
-    TOKEN = os.environ.get("TOKEN", "7927476758:AAHIGxCEbQP9yC5c8C8fzxU3SrAQ82oyf1o")
-    MONGO_URL = "mongodb+srv://TEAMBABY01:UTTAMRATHORE09@cluster0.vmjl9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-    START_PIC = "https://files.catbox.moe/z2ajhe.jpg"
-    SUDOERS = filters.user(["7400383704"])
+    
+    TOKEN = os.environ.get("TOKEN")
+    MONGO_URL = "mongodb+srv://Krishna:pss968048@cluster0.4rfuzro.mongodb.net/?retryWrites=true&w=majority"
+    START_PIC = "https://envs.sh/kU9.jpg"
+    SUDOERS = filters.user(["5738579437"])
