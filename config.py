a18525d5536100a7639918a37181e76a8aa103a2
Rishabh <152057079+Rishabhopi@users.noreply.github.com>
2024-12-16 07:51:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ class Config:
     API_ID = "20948838"
     API_HASH = "0d81af6bc752fd069824375a9d668839"
     
-    TOKEN = os.environ.get("TOKEN")
+    TOKEN = os.environ.get("TOKEN","6956731651:AAGt_HkoQFhhnuyx3P6srgirrweSSQi6ltA")
     MONGO_URL = "mongodb+srv://Krishna:pss968048@cluster0.4rfuzro.mongodb.net/?retryWrites=true&w=majority"
     START_PIC = "https://envs.sh/kU9.jpg"
     SUDOERS = filters.user(["5738579437"])
